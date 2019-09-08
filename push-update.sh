60a42b711cffbd611636d687180259b54874b1d1
McFateM <mcfatem@grinnell.edu>
2019-09-08 09:17:14 -0500

    Blog moved to Docker Hub summittdweller/weblog-sd

--- push-update.sh
+++ push-update.sh
@@ -3,5 +3,5 @@ cd ~/Projects/weblog-sd
 perl -i.bak -lpe 'BEGIN { sub inc { my ($num) = @_; ++$num } } s/(build = )(\d+)/$1 . (inc($2))/eg' config.toml
 docker image build -t blog-update .
 docker login
-docker tag blog-update mcfatem/weblog-sd:latest
-docker push mcfatem/weblog-sd:latest
+docker tag blog-update summittdweller/weblog-sd:latest
+docker push summittdweller/weblog-sd:latest
